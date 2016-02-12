d3ee8ae28ca08704de834606fda905dd42619b51
Dimitriio <dimitri.reynie@gmail.com>
2016-02-12 16:44:20 +0100

    commit

--- src/MyAlgorithm.cpp
+++ src/MyAlgorithm.cpp
@@ -79,6 +79,31 @@ double MyAlgorithm::fitness(const unsigned int index) const
 	return _fitness_values[index].fitness;
 }
 
+double MyAlgorithm::best_cost() const
+{
+	return _fitness_values[_upper_cost];
+}
+
+double MyAlgorithm::worst_cost() const
+{
+	return _fitness_values[_lower_cost];
+}
+
+Solution& MyAlgorithm::best_solution() const
+{
+	return _solutions[_upper_cost];
+}
+
+Solution& MyAlgorithm::worst_solution() const
+{
+	return _solutions[_lower_cost];
+}
+
+void evolution(int iter)
+{
+	 /*makes an evolution step*/
+}
+
 std::vector<Solution*> createSolutionVector()
 {
 	std::vector<Solution*> sol;
