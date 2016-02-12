d07133febb257a7ffdec205ff0583c82d0d07904
Dimitriio <dimitri.reynie@gmail.com>
2016-02-12 09:31:10 +0100

    merge

--- src/Function.cpp
+++ src/Function.cpp
@@ -14,7 +14,7 @@
 Function::~Function() {
 }
 
-double Function::rosenbrock(std::vector<double> X) {
+double rosenbrock(std::vector<double> X) {
 	double sum=0;
 	unsigned int dimension = X.size();
 	for (int i=0; i<dimension-1; ++i)
@@ -22,7 +22,7 @@ double Function::rosenbrock(std::vector<double> X) {
 
 	return sum;
 }
-double Function::ackley(std::vector<double> solution){
+double ackley(std::vector<double> solution){
 	double c=2*M_PI,carre =2;
 	unsigned int dim= solution.size();
 	double sum1=0;
@@ -38,7 +38,7 @@ double Function::ackley(std::vector<double> solution){
 	resultat=-20*exp(-0.2 * sqrt(1.0/dim * sum1))-exp(1.0/dim*sum2)+ 20+ exp(1.0);
 	return resultat;
 }
-double Function::schwefel(std::vector<double> solution){
+double schwefel(std::vector<double> solution){
 
 	unsigned int dim= solution.size();
 	double sum1=0;
@@ -51,7 +51,7 @@ double Function::schwefel(std::vector<double> solution){
 	resultat=418.9829*dim-(sum1);
 	return resultat;
 }
-double Function::rastrigin(std::vector<double> solution){
+double rastrigin(std::vector<double> solution){
 
 	unsigned int dim= solution.size();
 	double a=10;
@@ -64,15 +64,49 @@ double Function::rastrigin(std::vector<double> solution){
 	double resultat=a*dim+sum1;
 	return resultat;
 }
+double schaffer (std::vector<double> solution){
+
+	unsigned int dim= solution.size();
+		double sum = 0;
+		double innerThing = 0;
+
+		for(int i = 0; i < dim-1; i++)
+		{
+			innerThing = pow(solution[i], 2) + pow(solution[i+1], 2);
+			sum += pow(innerThing, 0.25) * (1 + pow(sin(50*pow(innerThing, 0.1)), 2));
+		}
+
+		double resultat=sum;
+		return resultat;
+
+}
+double weierstrass(std::vector<double> solution) {
+
+	// from random google code
+
+	unsigned int dim = solution.size();
+	double a = 0.5;
+	double b = 3.0;
+	int kmax = 20;
+
+	double f = 0;
+	for (int i = 0; i < dim; i++)
+		for (int k = 0; k <= kmax; k++)
+			f += pow(a, k) * cos(2 * M_PI * pow(b, k) * (solution[i] + 0.5));
+
+	return f;
+}
 
 double Function::launchFunction(std::vector<double> X, unsigned int n)
 {
-	switch(n)
+	switch(n){
 		case 1 : return rosenbrock(X); break;
 		case 2 : return ackley(X); break;
 		case 3 : return schwefel(X); break;
 		case 4 : return rastrigin(X); break;
 		case 5 : break;
 		case 6 : break;
-		default; break;
+		default: break;
+	}
 }
+
